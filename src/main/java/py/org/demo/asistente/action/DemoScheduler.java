ee664dafaaeb02eb18cdfcad55eb0adcebcc275f
Ale Feltes <awi@alefq.com>
2013-07-30 18:12:52 -0400

    Demo del demoiselle-scheduler

---
+++ src/main/java/py/org/demo/asistente/action/DemoScheduler.java
@@ -0,0 +1,38 @@
+package py.org.demo.asistente.action;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import br.gov.component.demoiselle.scheduler.ISchedulerAction;
+import br.gov.component.demoiselle.scheduler.Scheduler;
+import br.gov.component.demoiselle.scheduler.SchedulerActionManager;
+import br.gov.frameworkdemoiselle.annotation.Name;
+import br.gov.frameworkdemoiselle.lifecycle.Startup;
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+
+@Scheduler(expression = "16:27:00 EVERY_MINUTE")
+@Name("demoScheduler")
+@BusinessController
+public class DemoScheduler implements ISchedulerAction, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7185856082262566555L;
+	@Inject
+	Logger log;
+
+	@Startup
+	public void initialize() {
+		SchedulerActionManager scheduler;
+		scheduler = new SchedulerActionManager();
+		scheduler.execute();
+	}
+
+	public void execute() {
+		log.debug("Ejecutando tarea programada...");
+	}
+}
---
+++ src/main/java/py/org/demo/demoiselle/scheduler/persistence/BookmarkDAO.java
@@ -0,0 +1,13 @@
+package py.org.demo.demoiselle.scheduler.persistence;
+
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+import py.org.demo.demoiselle.scheduler.domain.Bookmark;
+
+@PersistenceController
+public class BookmarkDAO extends JPACrud<Bookmark, Long> {
+	
+	private static final long serialVersionUID = 1L;
+	
+}
